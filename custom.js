5cf39f2e9030e42bef6cf138519bb6ed223b5258
Muhammad-Hamza-Tahir <hamzatahir8935@gmail.com>
2021-12-02 13:04:02 +0500

    Update custom.js

--- custom.js
+++ custom.js
@@ -4,7 +4,12 @@ btn2 = document.querySelector('.btn2');
 
 
 id1.addEventListener('click',function () {
-    main1.classList.add('abc')
+    if ( main1.classList.contains('abc')) {
+        main1.classList.remove('abc')
+    }else{
+        main1.classList.add('abc')
+    }
+    
 })
 
 btn2.addEventListener('click',function () {
@@ -13,4 +18,11 @@ btn2.addEventListener('click',function () {
 })
 
 
+// function getValue() {
+//     main1.classList.add('abc') 
+// }
 
+
+
+// id1.addEventListener('click', getValue() )
+    
